707e4c9b0308bbbe883db940e93d989d1968a131
Awaab Abdelbagi <92119191+2waab@users.noreply.github.com>
2023-02-05 02:52:16 +0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -7,3 +7,25 @@ skils.forEach((el) => {
         });
     });
 });
+
+let project = document.querySelector(".project");
+let lengthOfRespo = document.querySelector(".respo");
+
+fetch("https://api.github.com/users/2waab/repos")
+.then((res) => res.json())
+.then(respo => {
+    lengthOfRespo.innerHTML = respo.length;
+    for (let i = 0; i < respo.length; i++) {
+        console.log(respo[i].url)
+        let card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = `
+            <img src="IMG/Subtract.svg" alt="Islamc vector" />
+            <div class="text">
+                <h4>${respo[i].name}</h4>
+                <a href="https://github.com/2waab/${respo[i].name}" target="_Blank">Show Project</a>
+            </div>
+        `;
+        project.appendChild(card);
+    };
+});
